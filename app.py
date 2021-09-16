0c4fc027faddee0e5d11dd14ed3878ff6b65d34a
Joel <ricci.lob@gmail.com>
2021-09-16 12:42:51 -0700

    External script to `ngl`

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ from dash_bootstrap_templates import load_figure_template
 # Template
 load_figure_template("journal")
 
+# External JS
+external_scripts = [
+    "https://unpkg.com/ngl@0.10.4/dist/ngl.js"
+]
+
 app = dash.Dash(
     __name__,
     suppress_callback_exceptions=True, 
     meta_tags=[{'name': 'viewport',
                 'content': 'width=device-width, initial-scale=1.0'
                 }],
-    external_stylesheets=[dbc.themes.JOURNAL]
+    external_stylesheets=[dbc.themes.JOURNAL],
+    external_scripts=external_scripts
 )
 app.title = 'JRL: LatinXChem poster'
 server = app.server
