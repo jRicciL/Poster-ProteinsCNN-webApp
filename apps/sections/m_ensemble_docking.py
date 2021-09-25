71e3b30215e2db5abe12dd99dabcf11579de984c
Joel <ricci.lob@gmail.com>
2021-09-24 20:48:06 -0700

    Ensemble docking section: Temporal version (static)

--- apps/sections/m_ensemble_docking.py
+++ apps/sections/m_ensemble_docking.py
@@ -2,6 +2,7 @@ import dash.html as html
 import dash.dcc as dcc
 import dash_bootstrap_components as dbc
 
+from app import app
 
 # Titles
 row_titles = dbc.Row(
@@ -25,16 +26,48 @@ row_texts = dbc.Row(
     className='row-text-content',
     children= [
        dbc.Col(
-           lg=6, md=6, sm=12,
+           lg=12, md=12, sm=12,
            children=[
-             'ola'
+             html.P(
+                [
+                    html.I(className='ico fas fa-circle s2'),
+                    html.Mark("AUC-ROC"),
+                    ' was used to evaluate the ',
+                    html.B('docking performance'),
+                    ' of ',
+                    html.Mark('each conformation.'),
+                ], #className='ident-1'
+            ),
+           html.P(
+                [
+                    html.I(className='ico fas fa-circle s2'),
+                    'Conformations with ',
+                    html.Code(html.Mark('AUC ROC > 0.7')),
+                    ' were labeled as ',
+                    html.Mark('"Druggables".', className='pill pill-red')
+                ], className='ident-1'
+            ),
            ]
        ),
        dbc.Col(
-           lg=6, md=6, sm=12,
+           lg=12, md=12, sm=12,
            children=[
-
-           ]
+               html.Hr(),
+               html.Br(),
+               html.H5(
+                   [
+                    'AUC-ROC:',
+                    html.Span(' SBVS performance per conformation',
+                        style={'font-weight': '300'}
+                    ),
+                   ]
+                ),
+               html.Img(
+                alt='SBVS AUC-ROC per conformation',
+                src=app.get_asset_url('images/auc_roc_per_conformation.png'),
+                className='img-fluid max-width mx-auto',
+            )
+           ], className='text-center'
            
        )
     ]
